4611b556f0c3c27496c639bbab77b6d37a53a7a8
Aldo Garcia <AldoGarcia1997@gmail.com>
2021-10-26 15:33:08 -0400

    Update PlayerClass: Testing functions, Adds Attack Shield, GetAction, GetShield, Fix Function
    +Add myHand and MyGY (String-friendly version of myHand/myGY. Mostly for testing purposes)
    +Add Attack Opponent
    +Add Attack Shield (Function that will attack and inflict damage to target player's shield) and returns remaining damage
    +Add GetAction (Increases Action by a)
    +Add getShield (Appends a Shield of value n to Player)
    +Add returnDeckk function
    +Fix Helper Functions ('[]' was used instead of '()')
    +Add a print to "DrawCard" to indicate there are no cards left in the deck.

--- PlayerClass.py
+++ PlayerClass.py
@@ -24,7 +24,17 @@ class Player:
 #           (May be a List of Integers, and if the game detects
 #           A player's len(Shield) is > 0, they may have to 
 #           choose which shield to deal damage to.)
-
+    def myHand(self):
+        myList = []
+        for n in self.Hand:
+            myList.append(str(n))
+        return myList
+    def myGY(self):
+        myList = []
+        for n in self.GY:
+            myList.append(str(n))
+        return myList
+#For testing Purposes
 ##################################################################
 #   Card Functions  (Currently in Player class as it seemed like it 
 #                   made the most sense as the actions in the game
@@ -32,17 +42,34 @@ class Player:
     def DrawCard(self, num=1):
         for i in range(0, num):
             if len(self.Deck) == 0:
+                print("     There are 0 cards in deck; Shuffling")
                 self.returnDeck()
             self.Deck2Hand() #
     def AttackOP(self, target, d): #Inflict {d} damage to Player {target}.
-        return 0
+        if len(target.Shield) > 0:
+            #print("     target's shields: {}".format(target.Shield))
+            return False
+        self.ChangeHP(-d, target)
+        return True
+    def AttackSh(self, target, d, st):
+        target.Shield[st] -= d
+        if target.Shield[st] == 0:
+            target.Shield.pop(st)
+            return d
+        if target.Shield[st] < 0:
+            return target.Shield.pop(st) * -1
+        else:
+            return 0
+
     def HealingT(self, h): #Restores {h} points of life to self.
                            #(Without going over Player.MaxHP)
-        return 0
+        self.ChangeHP(h, self)
+
     def GetAction(self, a): #Increase actions this turn by {a}.
-        return 0
+        self.Actions+=a
     def getShield(self, n): #Adds a shield of {n}.
-        return 0
+        self.Shield.append(n)
+    
 ####################################################################
 #   Helper Functions
 #   Since so many actions will be repeated in one way or another with each effect
@@ -52,21 +79,26 @@ class Player:
     def Deck2Hand(self, card = 0): 
         self.Hand.append(self.Deck.pop(card))
     def Hand2GY(self, card = 0):  
-        self.GY.append(self.Hand.pop[card])
+        self.GY.append(self.Hand.pop(card))
     def Deck2GY(self, card= 0):
-        self.GY.append(self.Deck.pop[card])
+        self.GY.append(self.Deck.pop(card))
     def GY2Deck(self, card= 0):
-        self.Deck.append(self.GY.pop[card])
-    def ChangeHP(self, n, target = self):
+        self.Deck.append(self.GY.pop(card))
+    def ChangeHP(self, n, target):
         target.HP += n
         if (target.HP > target.MHP):
             target.HP = target.MHP
+        if (target.HP < 0):
+            target.HP = 0
     
 
 #   Returns cards from the GY to the Deck, and proceeds to shuffle them
     def returnDeck(self):
+        print("     Shuffling Deck!")
         while len(self.GY) > 0:
-            self.GY2Deck()
+            #print("         {}".format(self.GY[0]))
+            self.GY2Deck(0)
+        print("GY: {}".format(self.GY))
         self.deckShuffle()
 #   Moves around the order of the Deck List, randomizing it.
     def deckShuffle(self):
