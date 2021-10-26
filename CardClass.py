f387c6b2119393706e6d21f89e80e69dd6c1178e
Aldo Garcia <AldoGarcia1997@gmail.com>
2021-10-26 15:27:06 -0400

    Update CardClass. Adds testing functions
    Update Card Class
    +Corrects typo in __init__ (defese to defense)
    +Adds __str__ function
    +Adds getList function

--- CardClass.py
+++ CardClass.py
@@ -1,14 +1,22 @@
 class Card:
     def __init__(self):
         self.data = []
-    def __init__(self, Name, Desc, Color, ID, Armor = 0, Draw= 0, Dmg = 0, ExtraA= 0, Heal = 0, PowerA = False, PowerB = False, PowerC = False):
+    #def __init__(self, Name, Desc, Color, ID, Armor = 0, Draw= 0, Dmg = 0, ExtraA= 0, Heal = 0, PowerA = False, PowerB = False, PowerC = False):
+    def __init__(self, Name, Desc, Color, ID, Dmg = 0, Draw= 0, Heal = 0, Armor = 0, ExtraA= 0, PowerA = False, PowerB = False, PowerC = False):
         self.name = Name
         self.desc = Desc
         self.id = ID
         self.damage= Dmg
-        self.defese= Armor
+        self.defense= Armor
         self.draw = Draw
         self.action= ExtraA
         self.healing= Heal
         self.powerA,self.powerB,self.powerC = PowerA, PowerB, PowerC
         self.color= Color
+        #print("ID: {} Damage: {}, Draw: {}, Action: {}, Armor: {}, Healing: {}".format(self.id, self.damage, self.draw, self.action, self.defense, self.healing))
+    def __str__(self):
+        return self.name
+    def getList(self):
+        return [self.name, self.desc, self.damage, self.draw, self.healing, self.action, self.defense]
+        #To get data easily to program
+        #Name, Description, Damage, Draw, Healing, Action, Armor
