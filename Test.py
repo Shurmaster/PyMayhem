1174a2b7b6aad5b727a3302912209b4bc10219ad
finley-talley <73396510+finley-talley@users.noreply.github.com>
2021-11-23 19:41:45 -0500

    adding some comments for clarity

--- Test.py
+++ Test.py
@@ -9,32 +9,45 @@ def UseCard(ply, n):
 
 
 if __name__ == "__main__":
+    # creating the original players
     P1 = Player("Hero", 2)
     P2 = Player("Rival", 1)
     inGame = True
+
     myL = (P1, P2)
     ind = 0
     for i in myL:
         for n in range(0,27):
+            # adding cards to each players deck
             myRand = random.randint(0, 30)                                                                            #Dmg = 0,             Draw= 0,              Heal = 0,             Armor = 0,            ExtraA= 0
             toAdd = Card("{}".format(myRand), "Is the {}th card.".format(myRand), i.deckID, random.randint(100, 200), random.randint(0, 4), random.randint(0, 1), random.randint(0, 1), random.randint(0, 2), int(random.randint(0, 50)/33) )
             i.Deck.append(toAdd)
+
+    # moves 3 cards from deck to hand
     turn = 1
     for i in myL:
         i.DrawCard(3)
 
+    # start of game
     while(inGame):
+
+        # start of turn
         for turnP in myL:
             if inGame == False:
                 break
             turnP.Actions += 1
+
             print("==== Turn {} ======".format(turn))
+            # draw 1 card every turn
             turnP.DrawCard(1)
             print("{} - HP:{} Deck:{}".format(turnP.player, turnP.HP, len(turnP.Deck)))
+
             Ptarget = myL[(myL.index(turnP) + 1) % len(myL)]
             print("Hand({}): {}".format(len(turnP.Hand), turnP.myHand()))
             print("GY({}): {}".format(len(turnP.GY), turnP.myGY()) )
             print("Armor: {}".format(turnP.Shield) )
+
+
             while turnP.Actions > 0:
                 myUsed= UseCard(turnP, random.randint(0, len(turnP.Hand) - 1))
                 print("Player {} used {} whichs deals {} Damage, Draws {} cards, Adds {} Armor, gives {} extra actions and Heals {} points.".format(turnP.player, myUsed, myUsed.damage, myUsed.draw, myUsed.defense, myUsed.action, myUsed.healing))
@@ -72,7 +85,6 @@ if __name__ == "__main__":
                     print("\nBy turn {}: {}'s Health is {}; {} wins with {} HP!".format(turn, Ptarget.player, Ptarget.HP, turnP.player, turnP.HP))
                     break
             turn += 1
-        
+
 
 #    def __init__(self, Name, ID):
-
