24b2aa4dd865bd253bac62bafe0ea72d763953b0
finley-talley <73396510+finley-talley@users.noreply.github.com>
2021-11-23 20:18:20 -0500

    multiple player rectangles drawn, screen res increased

--- Game.py
+++ Game.py
@@ -7,7 +7,7 @@ pg.init()
 bg = pg.image.load("images/start_saver_600x600.jpg")
 
 ## Can have a separate file for globals if we want
-SCREENWIDTH = 600
+SCREENWIDTH = 1200
 SCREENHEIGHT = 600
 
 
@@ -18,7 +18,7 @@ class Game:
         #self.player2 = Player()
         self.gameRunning = True
         self.gameState = 'start'
-    
+
     # starting up the screen
     def start(self):
         while self.gameRunning:
@@ -53,7 +53,7 @@ class Game:
                 self.gameState = 'playing'
             if event.type == pg.KEYDOWN and event.key == pg.K_TAB: # we'll have this to display options/help/about menu
                 self.gameState = 'help'
-    
+
     def start_draw(self):
         self.screen.blit(bg, (0, 0))
         self.draw_text('Dungeon Mayhem', self.screen, [SCREENWIDTH//2, SCREENHEIGHT//3], 32, pg.Color("white"), pg.font.get_default_font(), True)
@@ -82,7 +82,7 @@ class Game:
         self.draw_text("Placeholder", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//1.4], 24, pg.Color("white"), pg.font.get_default_font(), True)
         self.draw_text("Press SPACE to go back or Press ESCAPE to quit", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//1.2], 24, pg.Color("white"), pg.font.get_default_font(), True)
         pg.display.update()
-    
+
     ###################################### GAME PLAY HELPER FUNCTIONS #####################################
     def playing_events(self): # here is where we can have the keybinds and things for the actual gameplay
         for event in pg.event.get(): # like X is to play a card or whatever
@@ -91,11 +91,15 @@ class Game:
             if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
-    
+
     def playing_draw(self):
-        self.screen.fill(pg.Color("black"))
-        self.draw_text("This screen will be for the game", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//3], 24, pg.Color("white"), pg.font.get_default_font(), True)
-        self.draw_text("made it to the game!", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//2.5], 24, pg.Color("white"), pg.font.get_default_font(), True)
+        self.screen.fill(pg.Color("white"))
+        # self.draw_text("This screen will be for the game", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//3], 24, pg.Color("white"), pg.font.get_default_font(), True)
+        # self.draw_text("made it to the game!", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//2.5], 24, pg.Color("white"), pg.font.get_default_font(), True)
+
+
+        for i in range(0, 6):
+            pg.draw.rect(self.screen, "black", pg.Rect(100+(170*i), 400, 150, 200))
         pg.display.update()
 
 
