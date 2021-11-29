9fec3f5133507ec89efe1bbaac5b73be05e529e5
finley-talley <73396510+finley-talley@users.noreply.github.com>
2021-11-29 14:30:48 -0500

    added mechanic to select and confirm which shield
    can't test yet.

--- Game.py
+++ Game.py
@@ -20,7 +20,8 @@ class Game:
         self.screen = pg.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
         self.gameRunning = True
         self.gameState = 'start'
-        self.selectedCard = ['', pg.K_0]
+        self.selectedCard = []
+        self.selectedShield = []
 
         # info for each player
         self.P1 = YellowPlayer("Player 1") # deck ID 1 will be yellow
@@ -57,6 +58,9 @@ class Game:
             elif self.gameState == 'select shield':
                 self.shield_select_events()
                 self.shield_select_draw()
+            elif self.gameState == 'confirm shield':
+                self.confirm_shield_events()
+                self.confirm_shield_draw()
             elif self.gameState == 'help':
                 self.help_events()
                 self.help_draw()
@@ -256,6 +260,13 @@ class Game:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.gameRunning = False
+            if event.type == pg.KEYDOWN and (event.key in [pg.K_0, pg.K_1, pg.K_2, pg.K_3, pg.K_4, pg.K_5, pg.K_6]):
+                if int(pg.key.name(event.key)) in range(1, len(self.defender.shield)+1):
+                    print ("you selected key {}\n".format(pg.key.name(event.key)))
+                    # change the selected key
+                    self.selectedShield[0] = pg.key.name(event.key)
+                    self.selectedShield[1] = event.key
+                    self.gameState = 'confirm shield'
             if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
@@ -281,6 +292,56 @@ class Game:
 
         #self.draw_text("You selected card {}".format(self.selectedCard[0]), self.screen, [SCREENWIDTH//2, SCREENHEIGHT//2], 24, pg.Color("red"), pg.font.get_default_font(), True)
         self.draw_text("Now it's time to select an opponent shield.", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//2], 24, pg.Color("red"), pg.font.get_default_font(), True)
+        pg.display.update()
+
+    def confirm_shield_events(self):
+        # change your shield selection or confirm it with ENTER
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.gameRunning = False
+            if event.type == pg.KEYDOWN and (event.key in [pg.K_0, pg.K_1, pg.K_2, pg.K_3, pg.K_4, pg.K_5, pg.K_6]):
+                if int(pg.key.name(event.key)) in range(1, len(self.defender.shield)+1):
+                    print ("you selected key {}\n".format(pg.key.name(event.key)))
+                    # change the selected key
+                    self.selectedShield[0] = pg.key.name(event.key)
+                    self.selectedShield[1] = event.key
+                    self.gameState = 'confirm shield'
+            if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
+
+                self.attacker = self.players[self.turn % 2]
+                self.defender = self.players[(self.turn + 1) % 2]
+                self.attacker.take_turn_game(opponent=self.defender, hand_choice=(int(self.selectedCard[0])-1), opp_choice=int(selectedShield[1])+1)
+                self.turn += 1
+
+                # if the opponent's HP is zero, then game over
+
+                self.gameState = 'end turn'
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                pg.quit()
+                exit()
+
+    def confirm_shield_draw(self):
+        self.screen.fill(pg.Color("white"))
+
+        # drawing groups of shields
+        for i in range(0, len(self.defender.shield)):
+            for j in range(0, self.defender.shield[i]):
+                pg.draw.rect(self.screen, "pink", pg.Rect(120+(120*i)+(35*j), 0, 30, 30))
+
+        # # TODO: for cards in opponent's handl
+        # for i in range(0, 6):
+        #     # TODO: if card has shield, draw shield
+        #     pg.draw.rect(self.screen, "pink", pg.Rect(150+(150*i), 0, 100, 133))
+
+        # TODO: if card has shield, draw shield
+        for i, j in enumerate(self.currentPlayer.hand):
+            img = pg.image.load("images/{}/{}.jpg".format(j.deck, j.id)).convert()
+            rect = img.get_rect()
+            rect.topleft = (100+(170*i),350)
+            self.screen.blit(img, rect)
+
+        self.draw_text("You selected shield {}".format(self.selectedShield[0]), self.screen, [SCREENWIDTH//2, SCREENHEIGHT//2], 24, pg.Color("red"), pg.font.get_default_font(), True)
+        self.draw_text("Press ENTER to confirm your selection, or a number to select another shield.", self.screen, [SCREENWIDTH//2, SCREENHEIGHT//1.8], 24, pg.Color("red"), pg.font.get_default_font(), True)
         pg.display.update()
 
 
